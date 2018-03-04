1541e7e0a134c10f921d24d0f00ce3e5081f3ce6
DevelopGenius <maximpidchenko@gmail.com>
2018-03-04 20:45:50 +0200

    created android application

---
+++ app/src/main/java/com/example/user/helpingsociety/MainPage.java
@@ -0,0 +1,15 @@
+package com.example.user.helpingsociety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.database.sqlite.*;
+
+public class MainPage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
